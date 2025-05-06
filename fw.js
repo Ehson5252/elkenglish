47b1ef5d53a76ff528fa9382d6ef26f40ad22e8a
Ekhsoni Olimzoda <oehsoni1@gmail.com>
2025-05-06 12:13:32 -0500

    Added new words. Total 15.  Wrote new algorithm.

--- fw.js
+++ fw.js
@@ -1,101 +1,168 @@
 import words_data from "./modules/words_data.js";
 
-let topNumsforWords = document.querySelector(".numsForWords")
+let indicators = document.querySelector(".indicators")
 let answer1 = document.getElementById("answer1");
 let answer2 = document.getElementById("answer2");
 let answer3 = document.getElementById("answer3");
 let answer4 = document.getElementById("answer4");
 let next = document.getElementById("next");
 let img = document.getElementById("img");
-let comment_text = document.getElementById("wellDone");
+let comment_text = document.getElementById("comment_text");
 
-let p = topNumsforWords.children
+let right_comment = "Right! Дуруст!";
+let wrong_comment = "Wrong!!! Нодуруст!!!";
 
-
-let right_comment = "Дуруст!!!";
-let wrong_comment = "Нодуруст!!!";
-
-const list = [answer1, answer2, answer3, answer4];
-let copyArr = [...words_data]
-let wordsLeft = [...words_data]
-
+let amountOfQs = 10;
 let randomNumOf4;
-let randomNumOfAll;
-let theR;
-let numOfQuestion = 0
+let theROption;
+let indexOfCurQ = 0
+let rightAs = 0;
 
-p[numOfQuestion].style.transform ="scale(1.2)"
+const answers = [answer1, answer2, answer3, answer4];
+let copyArr = [...words_data]
+let checkArr = []
 
-const call1 = () => {
-    randomNumOf4 = Math.floor(Math.random()*4);
-    randomNumOfAll = Math.floor(Math.random()*wordsLeft.length);
-
-    theR =  wordsLeft[randomNumOfAll].word;
-
-    list[randomNumOf4].innerText = theR
-    img.src= wordsLeft[randomNumOfAll].img;
-
-    wordsLeft.splice(randomNumOfAll, 1);
-
-    let findIndex = copyArr.findIndex(e => e.word == theR);
-    copyArr.splice(findIndex, 1);
-}
-
-const call2 = ()=> {
-    for(let i=0; i<list.length; i++) {
-        if(i!==randomNumOf4) {
-            let rand = Math.floor(Math.random() * copyArr.length);
-            list[i].innerText = copyArr[rand].word;
-            copyArr.splice(rand, 1);
-        }
+const makeIndicators = (amountOfQs) => {
+    for(let i=1; i<=amountOfQs; i++) {
+        let hElm = document.createElement("p")
+        let node = document.createTextNode(i);
+        hElm.appendChild(node);
+        indicators.appendChild(hElm)  
     }
 }
 
-call1();
-call2();
+makeIndicators(amountOfQs);
+
+let ind = indicators.children;
+ind[indexOfCurQ].style.transform ="scale(1.2)"
 
 
-answer1.addEventListener('click', () => func(list[0]));
-answer2.addEventListener('click', () => func(list[1]));
-answer3.addEventListener('click', () => func(list[2]));
-answer4.addEventListener('click', () => func(list[3]));
+const makeRandomQs = (originalQs, qty) => {
+    let randomQs = [];
+    let usedIdexes = new Set();
+
+    while (randomQs.length<qty && usedIdexes.size<originalQs.length) {
+        console.log("repeat");
+        let randomIndex = Math.floor(Math.random() * originalQs.length);
+        if(!usedIdexes.has(randomIndex)) {
+            randomQs.push(originalQs[randomIndex]);
+            usedIdexes.add(randomIndex)
+        } 
+    }
+    return randomQs;
+}
+
+
+let randomQs = makeRandomQs(copyArr, amountOfQs);
+
+
+const crtROption = () => {
+    console.log("crtROption")
+
+    theROption = randomQs[indexOfCurQ].word;
+
+    randomNumOf4 = Math.floor(Math.random()*4);
+    answers[randomNumOf4].innerText = theROption
+    img.src= randomQs[indexOfCurQ].img;
+    checkArr.push(theROption)
+}
+
+const crt3MoreOption = ()=> {
+    console.log("crt3MoreOption")
+
+    for(let i=0; i<answers.length; i++) {
+        console.log("for loop inside crt3MoreOption" )
+        if(i!==randomNumOf4) {
+            let rand = Math.floor(Math.random() * copyArr.length);
+
+            if(!checkArr.includes(copyArr[rand].word)) {
+                answers[i].innerText = copyArr[rand].word;
+                checkArr.push(copyArr[rand].word)
+
+            } else {
+                i--;
+            }
+            
+        }
+    }
+    checkArr = [];
+}
+
+crtROption();
+crt3MoreOption();
+
+
+answer1.addEventListener('click', () => func(answers[0]));
+answer2.addEventListener('click', () => func(answers[1]));
+answer3.addEventListener('click', () => func(answers[2]));
+answer4.addEventListener('click', () => func(answers[3]));
 next.addEventListener('click', () => nextFunc());
 
 
 
 
 function func(arg) {
-    console.log("clicked first button")
-    if(arg.innerText == theR) {
+    console.log(" inside func(arg) clicked ")
+
+    if(arg.innerText == theROption) {
+        console.log("right answer");
+
         comment_text.innerText=right_comment;
         comment_text.style.color = "green"
-        setTimeout(() => nextFunc(), 700)
-        p[numOfQuestion].style.transform ="scale(1)"
-        p[numOfQuestion].style.background ="green"
-        numOfQuestion++;
-        p[numOfQuestion].style.transform ="scale(1.2)"
+        ind[indexOfCurQ].style.transform ="scale(1)"
+        ind[indexOfCurQ].style.background ="green"
 
+        rightAs++;
+        indexOfCurQ++;
+
+        if(indexOfCurQ < amountOfQs) {
+            setTimeout(() => nextFunc(), 700)
+        } else {
+            console.log("do you ?  " + indexOfCurQ)
+            setTimeout(() => playAgain(), 1000) 
+        }
+        
     } else {
-        comment_text.innerText="Нодуруст!!!";
+        console.log("wrong answer");
+
+        comment_text.innerText = wrong_comment;
         comment_text.style.color = "red"
-        setTimeout(() => nextFunc(), 700)
-        p[numOfQuestion].style.transform ="scale(1)"
-        p[numOfQuestion].style.background ="red"
-        numOfQuestion++;
-        p[numOfQuestion].style.transform ="scale(1.2)"
+        ind[indexOfCurQ].style.transform ="scale(1)"
+        ind[indexOfCurQ].style.background ="red"
+
+        indexOfCurQ++;
+
+        if(indexOfCurQ < amountOfQs) {
+            setTimeout(() => nextFunc(), 700)
+        } else {
+            setTimeout(() => playAgain(), 1000) 
+        }
+
+       
     }
 }
 
 
-let counter = 4;
 
 function nextFunc() {
-    copyArr=[...words_data];
-    if(counter>0){
         comment_text.innerText = "";
-        console.log("yes", counter)
-        call1();
-        call2();
-        counter = counter-1;
-    }
+        crtROption();
+        crt3MoreOption();
+        
+        ind[indexOfCurQ].style.transform ="scale(1.2)"
+
+    
+        console.log("end of nexfunc");
+}
+
+function playAgain() {
+    comment_text.innerHTML = `
+        <p style="font-size:20px; color:#2400FF; ">You answered ${rightAs} question(s) correctly.</p>
+        <p style="font-size:16px; color:#2400FF; display: block "> Шумо ба ${rightAs} савол дуруст ҷавоб додед. </p>
+        <a style = "display: block; " href = 'http://elkenglish.net' >Play Again</a>
+    `
+    answer1.disabled = true;
+    answer2.disabled = true;
+    answer3.disabled = true;
+    answer4.disabled = true;
 }
--- modules/words_data.js
+++ modules/words_data.js
@@ -1,29 +1,68 @@
 
 const words_data = [
+
+    {
+        word: "book",
+        img: "assets/word's_photos/book.jpg"
+    },
+    {
+        word: "chair",
+        img: "assets/word's_photos/chair.jpg"
+    },
+    {
+        word: "door",
+        img: "assets/word's_photos/door.jpg"
+    },
+    {
+        word: "ear",
+        img: "assets/word's_photos/ear.jpg"
+    },
+    {
+        word: "flower",
+        img: "assets/word's_photos/flower.jpg"
+    },
+    {
+        word: "home",
+        img: "assets/word's_photos/home.jpg"
+    },
     {
         word: "mirror",
-        img: "assets/mirror.jpg"
+        img: "assets/word's_photos/mirror.jpg"
     },
     {
         word: "mouse",
-        img: "assets/mouse.jpg"
+        img: "assets/word's_photos/mouse.jpg"
     },
     {
         word: "mug",
-        img: "assets/mug.jpg"
-    },
-    {
-        word: "wallet",
-        img: "assets/wallet.jpg"
+        img: "assets/word's_photos/mug.jpg"
     },
     {
         word: "pen",
-        img: "assets/pen.jpg"
+        img: "assets/word's_photos/pen.jpg"
     },
     {
-        word: "book",
-        img: "assets/book.jpg"
-    }
+        word: "pencil",
+        img: "assets/word's_photos/pencil.jpg"
+    },
+    {
+        word: "wallet",
+        img: "assets/word's_photos/wallet.jpg"
+    },
+    {
+        word: "water",
+        img: "assets/word's_photos/water.jpg"
+    },
+    {
+        word: "window",
+        img: "assets/word's_photos/window.jpg"
+    },
+    {
+        word: "hand",
+        img: "assets/word's_photos/hand.jpg"
+    },
+    
+    
 ]
 
 export default words_data ;
